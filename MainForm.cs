6db6d1191c9cbede8619e7d6de3886d482708bd3
Aaron B <lavaaaron25@gmail.com>
2020-10-18 13:39:04 +0200

    Version 1.2 - The *broken* blend map update

--- MainForm.cs
+++ MainForm.cs
@@ -5,89 +5,165 @@ using System.Drawing.Imaging;
 using System.Windows.Forms;
 
 namespace HeightMapGen {
-    public partial class MainForm : Form {
+	public partial class MainForm : Form {
+		Bitmap heightBitmap, blendBitmap;
 
-        public MainForm() {
-            InitializeComponent();
-        }
+		public MainForm() {
+			InitializeComponent();
+		}
 
-        public double normalizeValue(double value, double max, double min, double newMax, double newMin) {
-            return (value - min) * (newMax - newMin) / (max - min) + newMin;
-        }
+		public double normalizeValue(double value, double max, double min, double newMax, double newMin) {
+			return (value - min) * (newMax - newMin) / (max - min) + newMin;
+		}
 
-        private void generateButton_Click(object sender, EventArgs e) {
-            if (widthInputBox.Value != 0 && heightInputBox.Value != 0) {
-                Random random = new Random();
-                Bitmap bitmap = new Bitmap(Convert.ToInt32(widthInputBox.Value), Convert.ToInt32(heightInputBox.Value));
+		private void generateButton_Click(object sender, EventArgs e) {
+			if (widthInputBox.Value != 0 && heightInputBox.Value != 0) {
+				Random random = new Random();
+				heightBitmap = new Bitmap(Convert.ToInt32(widthInputBox.Value), Convert.ToInt32(heightInputBox.Value));
+				blendBitmap = new Bitmap(Convert.ToInt32(widthInputBox.Value), Convert.ToInt32(heightInputBox.Value));
 
-                long seed = useRandomSeedCheckBox.Checked ? random.Next(int.MinValue, int.MaxValue) : Convert.ToInt64(seedInputBox.Value);
+				long seed = useRandomSeedCheckBox.Checked ? random.Next(int.MinValue, int.MaxValue) : Convert.ToInt64(seedInputBox.Value);
 
-                int coordinateMultiplier = 1;
-                int colorValue;
+				int coordinateMultiplier = 1;
+				int colorValue;
 
-                double finalMax = 1d;
-                double value;
+				double finalMax = 1d;
+				double value;
 
-                if (useFastLibraryRadioButton.Checked) {
-                    OpenSimplex2F noiseLibrary = new OpenSimplex2F(seed);
+				Text = "HeightMapGen - Working...";
 
-                    for (int x = 0; x < widthInputBox.Value; x++) {
-                        for (int y = 0; y < heightInputBox.Value; y++) {
-                            value = normalizeValue(noiseLibrary.Noise2(x / Convert.ToDouble(resolutionInputBox.Value), y / Convert.ToDouble(resolutionInputBox.Value)), 1d, -1d, 1d, 0d);
+				if (useFastLibraryRadioButton.Checked) {
+					OpenSimplex2F noiseLibrary = new OpenSimplex2F(seed);
 
-                            for (int o = 1; o - 1 < octavesInputBox.Value; o++) {
-                                finalMax += 1d / Math.Pow(2, o);
+					for (int x = 0; x < widthInputBox.Value; x++) {
+						for (int y = 0; y < heightInputBox.Value; y++) {
+							value = normalizeValue(noiseLibrary.Noise2(x / Convert.ToDouble(resolutionInputBox.Value), y / Convert.ToDouble(resolutionInputBox.Value)), 1d, -1d, 1d, 0d);
 
-                                value += 1d / Math.Pow(2, o) * normalizeValue(noiseLibrary.Noise2(Math.Pow(2, o) * (x / Convert.ToDouble(resolutionInputBox.Value)), Math.Pow(2, o) * (y / Convert.ToDouble(resolutionInputBox.Value))), 1d, -1d, 1d, 0d);
-                                coordinateMultiplier = coordinateMultiplier * 2;
-                            }
+							for (int o = 1; o - 1 < octavesInputBox.Value; o++) {
+								finalMax += 1d / Math.Pow(2, o);
 
-                            colorValue = (int) normalizeValue(value, finalMax, 0d, 255d, 0d);
-                            bitmap.SetPixel(x, y, Color.FromArgb(colorValue, colorValue, colorValue));
+								value += 1d / Math.Pow(2, o) * normalizeValue(noiseLibrary.Noise2(Math.Pow(2, o) * (x / Convert.ToDouble(resolutionInputBox.Value)), Math.Pow(2, o) * (y / Convert.ToDouble(resolutionInputBox.Value))), 1d, -1d, 1d, 0d);
+								coordinateMultiplier = coordinateMultiplier * 2;
+							}
 
-                            finalMax = 1d;
-                        }
-                    }
-                } else {
-                    OpenSimplex2S noiseLibrary = new OpenSimplex2S(seed);
+							mapValue(x, y, value, finalMax, Convert.ToDouble(exponentInputBox.Value));
+						}
+					}
+				} else {
+					OpenSimplex2S noiseLibrary = new OpenSimplex2S(seed);
 
-                    for (int x = 0; x < widthInputBox.Value; x++) {
-                        for (int y = 0; y < heightInputBox.Value; y++) {
-                            value = normalizeValue(noiseLibrary.Noise2(x / Convert.ToDouble(resolutionInputBox.Value), y / Convert.ToDouble(resolutionInputBox.Value)), 1d, -1d, 1d, 0d);
+					for (int x = 0; x < widthInputBox.Value; x++) {
+						for (int y = 0; y < heightInputBox.Value; y++) {
+							value = normalizeValue(noiseLibrary.Noise2(x / Convert.ToDouble(resolutionInputBox.Value), y / Convert.ToDouble(resolutionInputBox.Value)), 1d, -1d, 1d, 0d);
 
-                            for (int o = 1; o - 1 < octavesInputBox.Value; o++) {
-                                finalMax += 1d / Math.Pow(2, o);
+							for (int o = 1; o - 1 < octavesInputBox.Value; o++) {
+								finalMax += 1d / Math.Pow(2, o);
 
-                                value += 1d / Math.Pow(2, o) * normalizeValue(noiseLibrary.Noise2(Math.Pow(2, o) * (x / Convert.ToDouble(resolutionInputBox.Value)), Math.Pow(2, o) * (y / Convert.ToDouble(resolutionInputBox.Value))), 1d, -1d, 1d, 0d);
-                                coordinateMultiplier = coordinateMultiplier * 2;
-                            }
+								value += 1d / Math.Pow(2, o) * normalizeValue(noiseLibrary.Noise2(Math.Pow(2, o) * (x / Convert.ToDouble(resolutionInputBox.Value)), Math.Pow(2, o) * (y / Convert.ToDouble(resolutionInputBox.Value))), 1d, -1d, 1d, 0d);
+								coordinateMultiplier = coordinateMultiplier * 2;
+							}
 
-                            colorValue = (int) normalizeValue(value, finalMax, 0d, 255d, 0d);
-                            bitmap.SetPixel(x, y, Color.FromArgb(colorValue, colorValue, colorValue));
+							mapValue(x, y, value, finalMax, Convert.ToDouble(exponentInputBox.Value));
+							finalMax = 1d;
+						}
+					}
+				}
 
-                            finalMax = 1d;
-                        }
-                    }
-                }
+				heightMapImageBox.Image = heightBitmap;
+				blendMapImageBox.Image = blendBitmap;
 
-                if (saveFileDialog.ShowDialog() == DialogResult.OK) {
-                    bitmap.Save(saveFileDialog.FileName, ImageFormat.Png);
-                }
+				Text = "HeightMapGen";
+			}
+		}
 
-                bitmap.Dispose();
-            }
-        }
+		public void mapValue(int x, int y, double value, double finalMaximum, double exponent) {
+			value = Math.Pow(normalizeValue(value, finalMaximum, 0d, 1d, 0d), exponent);
 
-        private void useFastLibraryRadioButton_CheckedChanged(object sender, EventArgs e) {
-            useSmoothLibraryRadioButton.Checked = !useFastLibraryRadioButton.Checked;
-        }
+			if (value <= 0.33d) {
+				blendBitmap.SetPixel(x, y, Color.FromArgb((int) normalizeValue(value, 0.33d, 0d, 255d, 0d), 0, 0));
+			} else if (value <= 0.66d) {
+				blendBitmap.SetPixel(x, y, Color.FromArgb(0, (int) normalizeValue(value, 0.66d, 0.33d, 255d, 0d), 0));
+			} else {
+				blendBitmap.SetPixel(x, y, Color.FromArgb(0, 0, (int) normalizeValue(value, 1d, 0.66d, 255d, 0d)));
+			}
 
-        private void useSmoothLibraryRadioButton_CheckedChanged(object sender, EventArgs e) {
-            useFastLibraryRadioButton.Checked = !useSmoothLibraryRadioButton.Checked;
-        }
+			heightBitmap.SetPixel(x, y, Color.FromArgb((int) (value * 255), (int) (value * 255), (int) (value * 255)));
+		}
 
-        private void useRandomSeedCheckBox_CheckedChanged(object sender, EventArgs e) {
-            seedInputBox.Enabled = !useRandomSeedCheckBox.Checked;
-        }
-    }
+		private void useFastLibraryRadioButton_CheckedChanged(object sender, EventArgs e) {
+			useSmoothLibraryRadioButton.Checked = !useFastLibraryRadioButton.Checked;
+		}
+
+		private void useSmoothLibraryRadioButton_CheckedChanged(object sender, EventArgs e) {
+			useFastLibraryRadioButton.Checked = !useSmoothLibraryRadioButton.Checked;
+		}
+
+		private void useRandomSeedCheckBox_CheckedChanged(object sender, EventArgs e) {
+			seedInputBox.Enabled = !useRandomSeedCheckBox.Checked;
+		}
+
+		private void saveBlendmapButton_Click(object sender, EventArgs e) {
+			saveImage(blendBitmap);
+		}
+
+		private void saveHeightmapButton_Click(object sender, EventArgs e) {
+			saveImage(heightBitmap);
+		}
+
+		public void saveImage(Bitmap bitmap) {
+
+			switch (imageFormatComboBox.SelectedIndex) {
+				case 0:
+					saveFileDialog.DefaultExt = "png";
+					saveFileDialog.Filter = "png";
+					break;
+				case 1:
+					saveFileDialog.DefaultExt = "jpeg";
+					saveFileDialog.Filter = "jpeg";
+					break;
+				case 2:
+					saveFileDialog.DefaultExt = "tiff";
+					saveFileDialog.Filter = "tiff";
+					break;
+				case 3:
+					saveFileDialog.DefaultExt = "bmp";
+					saveFileDialog.Filter = "bmp";
+					break;
+				case 4:
+					saveFileDialog.DefaultExt = "png";
+					saveFileDialog.Filter = "png";
+					break;
+				default:
+					saveFileDialog.DefaultExt = "png";
+					saveFileDialog.Filter = "png";
+					break;
+			}
+
+			if (saveFileDialog.ShowDialog() == DialogResult.OK) {
+
+				switch (imageFormatComboBox.SelectedIndex) {
+					case 0:
+						bitmap.Save(saveFileDialog.FileName, ImageFormat.Png);
+						break;
+					case 1:
+						bitmap.Save(saveFileDialog.FileName, ImageFormat.Jpeg);
+						break;
+					case 2:
+						bitmap.Save(saveFileDialog.FileName, ImageFormat.Tiff);
+						break;
+					case 3:
+						bitmap.Save(saveFileDialog.FileName, ImageFormat.Bmp);
+						break;
+					case 4:
+						bitmap.Save(saveFileDialog.FileName, ImageFormat.Gif);
+						break;
+					default:
+						bitmap.Save(saveFileDialog.FileName, ImageFormat.Png);
+						break;
+				} 
+			}
+
+			bitmap.Dispose();
+		}
+	}
 }
